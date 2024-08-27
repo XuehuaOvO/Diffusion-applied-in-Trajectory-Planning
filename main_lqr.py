c1b05ebed3f27666e7454e4c717125b4291f44f8
Xuehua Xiao <xiaoxuehua.xh@gmail.com>
2024-08-27 14:42:10 +0200

    LQR Updating for Diffusion

--- main_lqr.py
+++ main_lqr.py
@@ -1,19 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import os
+import control as ctrl
 from pylab import *
 import casadi as ca
 from casadi.tools import *
+import torch
 
 ############### Seetings ######################
 
-num_datagroup = 300 # number of data groups 
 folder_path = "C:/Users/Xuehua Xiao/Desktop/S1_CartPole/lqr data collecting" # folder to save files
 
 # Time settings
-T = 3  # Total time (seconds)
-dt = 0.01  # Time step (seconds)
+T = 3.25  # Total time (seconds)
+dt = 0.05  # Time step (seconds)
+t = np.arange(0, T, dt)
 
-
+x_data_shape = (len(t)-1, 4)
+u_data_shape = (len(t)-1, 1)
 
 ############### LQR ######################
 
@@ -31,6 +35,15 @@ B = np.array([
     [0],
     [5]
 ])
+
+C = np.array([
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1]
+])
+
+D = np.array([[0],[0],[0],[0]])
 
 # Cost matrices
 Q = np.diag([10, 1, 10, 1])
@@ -71,21 +84,44 @@ print(P_sol.full())
 print("\nLQR Gain Matrix (K):")
 print(K)
 
+# check closed loop response
+# A_cl = A - np.dot(B, K)
+# sys = ctrl.ss(A_cl, B, C, D)
 
+# time, response = ctrl.step_response(sys)
 
-############### Control Loop ######################
+# num_outputs = response.shape[0]
+
+# for i in range(num_outputs):
+#     plt.plot(time, response[i, 0, :], label=f'Output {i+1}')
+
+# plt.xlabel('Time')
+# plt.ylabel('Response')
+# plt.legend()
+# plt.show()
+
+# ############### Control Loop ######################
+# # define variables to save collected x and u data
+# x_list = []
+# u_list = []
 
 # Define the initial states range
-rng_x = np.linspace(-1,1,20)
-rng_theta = np.linspace(-np.pi/4,np.pi/4,15)
+rng_x = np.linspace(-1,1,50) # 50 x_0 samples
+rng_theta = np.linspace(-np.pi/4,np.pi/4,50) # 50 theta_0 samples
 
 rng0 = []
 for m in rng_x:
     for n in rng_theta:
         rng0.append([m,n])
 rng0 = np.array(rng0,dtype=float)
+num_datagroup = len(rng0)
+num_iterations = num_datagroup
 
 # data collecting loop
+
+x_all_tensor = torch.zeros(num_iterations,*x_data_shape)
+u_all_tensor = torch.zeros(num_iterations,*u_data_shape)
+
 for turn in range(num_datagroup):
   
   num_turn = turn + 1
@@ -98,10 +134,11 @@ for turn in range(num_datagroup):
  
   #save the initial states
   x0 = np.array([x_0 , 0, theta_0, 0])  # Initial states
-  txtfile = 'initial states'
-  txt_name = txtfile + " " + num_turn_float + '.txt'
-  full_txt = os.path.join(folder_path, txt_name)
-  np.savetxt(full_txt, x0, delimiter=",",fmt='%1.3f')
+
+ #   txtfile = 'initial states'
+ #   txt_name = txtfile + " " + num_turn_float + '.txt'
+ #   full_txt = os.path.join(folder_path, txt_name)
+ #   np.savetxt(full_txt, x0, delimiter=",",fmt='%1.3f')
 
   # Convert K to CasADi MX
   K_casadi = ca.MX(K)
@@ -113,25 +150,31 @@ for turn in range(num_datagroup):
   state_update = ca.Function('state_update', [x], [xdot,u])
 
   # Simulation
-  t = np.arange(0, T, dt)
+  # t = np.arange(0, T, dt)
   x_hist = np.zeros((len(t), len(x0)))
   x_hist[0] = x0
-  u_hist = np.zeros(len(t))
+  u_hist = np.zeros(len(t)-1)
 
   for i in range(1, len(t)):
        xdot_val, u_val = state_update(x_hist[i-1])
        x_hist[i] = x_hist[i-1] + dt * xdot_val.full().flatten()
-       u_hist[i] = u_val.full().item()
+       u_hist[i-1] = u_val.full().item()
 
-  # Save the control inputs results to CSV files
-  cvsfile = 'u_data'
-  cvs_name = cvsfile + " " + num_turn_float + '.csv'
-  full_cvs = os.path.join(folder_path, cvs_name)
-  np.savetxt(full_cvs, u_hist, delimiter=",", fmt='%1.6f')
+  # save the (N-1) x and u 
+  x_save = x_hist[:-1,:]
+  u_save = u_hist
 
-  # Plot some results
-  import matplotlib.pyplot as plt
-  if turn in (0, 61, 134, 227, 295):
+  # convert to tensor
+  x_tensor = torch.tensor(x_save)
+  u_tensor = torch.tensor(u_save)
+  u_tensor = u_tensor.unsqueeze(1) # size [59] -> size [59, 1]
+
+  # save tensor in variables 
+  x_all_tensor[turn] = x_tensor
+  u_all_tensor[turn] = u_tensor
+
+  # some plot results
+  if turn in (0, 1125, 2466):
      plt.figure(figsize=(10, 8))
      plt.subplot(5, 1, 1)
      plt.plot(t, x_hist[:, 0])
@@ -150,7 +193,7 @@ for turn in range(num_datagroup):
      plt.ylabel('theta_dot (rad/s)')
      plt.grid()
      plt.subplot(5, 1, 5)
-     plt.plot(t, u_hist)
+     plt.plot(t[:-1], u_hist)
      plt.ylabel('u_value')
      plt.xlabel('Time (s)')
      plt.grid()
@@ -161,3 +204,13 @@ for turn in range(num_datagroup):
      plot_name = plotfile + " " + num_turn_float + '.png'
      full_plot = os.path.join(folder_path, plot_name)
      plt.savefig(full_plot)
+
+# convert list to tensor
+# x_all_tensor = torch.cat(x_list, dim=0)
+# u_all_tensor = torch.cat(u_list, dim=0)
+print(f'collecting states data size -- {x_all_tensor.size()}') # [50*50, 59, 4]
+print(f'collecting inputs data size -- {u_all_tensor.size()}') # [50*50, 59, 1] 
+
+# save collecting data in torch PT file
+torch.save(x_all_tensor, os.path.join(folder_path, f'x-collecting.pt'))
+torch.save(u_all_tensor, os.path.join(folder_path, f'u-collecting.pt'))
